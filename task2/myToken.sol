0590f9347c3ebb2bb6c7e4a70244170be00c5a98
aibaguoguo <491011130@qq.com>
2025-08-27 12:09:06 +0800

    作业 1：ERC20 代币

---
+++ task2/myToken.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+/*
+任务：参考 openzeppelin-contracts/contracts/token/ERC20/IERC20.sol实现一个简单的 ERC20 代币合约。要求：
+合约包含以下标准 ERC20 功能：
+balanceOf：查询账户余额。
+transfer：转账。
+approve 和 transferFrom：授权和代扣转账。
+使用 event 记录转账和授权操作。
+提供 mint 函数，允许合约所有者增发代币。
+提示：
+使用 mapping 存储账户余额和授权信息。
+使用 event 定义 Transfer 和 Approval 事件。
+部署到sepolia 测试网，导入到自己的钱包
+
+*/
+contract MyERC20 {
+
+    /**
+     * the list who keeps the token.
+     */
+    mapping(address account => uint256) private _keepers;
+
+    /**
+     * the permission list.
+     */
+    mapping(address account => mapping(address spender => uint256)) private _allowances;
+
+    /**
+     * the owner of the contract
+     */
+    address private _owner;
+
+    /**
+     * the total number of the token
+     */
+    uint256 private _totalSupply = 135888;
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(){
+        _owner = msg.sender;
+        _keepers[_owner] = _totalSupply;
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view virtual returns (string memory) {
+        return "YI Fu And Quick";
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view virtual returns (string memory) {
+        return "YFQ";
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the default value returned by this function, unless
+     * it's overridden.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view virtual returns (uint8) {
+        return 18;
+    }
+
+    /**
+     * @dev Returns the value of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256){
+        return _totalSupply;
+    }
+
+    /**
+     * @dev Returns the value of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256){
+        return _keepers[account];
+    }
+
+    /**
+     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 value) external returns (bool){
+        require(_keepers[msg.sender] >= value, "balance not enough");
+        _keepers[msg.sender] -= value;
+        _keepers[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256){
+        return _allowances[owner][spender];
+    }
+
+    /**
+     * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
+     * caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 value) external returns (bool){
+        _allowances[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev Moves a `value` amount of tokens from `from` to `to` using the
+     * allowance mechanism. `value` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 value) external returns (bool){
+        require(_keepers[from] >= value, "balance not enough");
+        require(_allowances[msg.sender][from] >= value, "allowance not enough");
+        _keepers[from] -= value;
+        _keepers[to] += value;
+        _allowances[msg.sender][from] -= value;
+        emit Transfer(from, to, value);
+        return true;
+
+    }
+}
